df9edc2c49dcf839616ec933ea920b120dff6bee
Naveen Kumar <59390882+Naveenkumar-cmd@users.noreply.github.com>
2024-06-18 14:22:14 +0530

    Update NewsList.vue

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,1 +1,24 @@
+<template>
+  <div>
+    <h2>News Articles</h2>
+    <div v-for="article in articles" :key="article.url" class="article">
+      <h3>{{ article.title }}</h3>
+      <p>{{ article.description }}</p>
+      <a :href="article.url" target="_blank">Read more</a>
+    </div>
+  </div>
+</template>
 
+<script>
+export default {
+  props: {
+    articles: Array
+  }
+}
+</script>
+
+<style scoped>
+.article {
+  margin-bottom: 20px;
+}
+</style>
