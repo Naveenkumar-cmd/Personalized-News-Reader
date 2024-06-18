1c74b62f452447a8530b2b0d1b579a5b483356e2
Naveen Kumar <59390882+Naveenkumar-cmd@users.noreply.github.com>
2024-06-18 14:23:51 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,24 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
 
+<script>
+import Home from '@/views/Home.vue';
+
+export default {
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
